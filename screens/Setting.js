093a940b2cac4dfab5579c7adc3c3735742e0096
AndrewBerland <116660276+AndrewBerland@users.noreply.github.com>
2024-02-11 23:05:18 -0500

    Update Setting.js

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, render } from 'react';
 import { View, Text, Switch } from 'react-native';
-import { styles, textStyles } from '../styles/styles';
+import { styles, textStyles, dark_mode } from '../styles/styles';
 import { StatusBar } from 'expo-status-bar';
 import FooterTab from '../components/FooterTab';
 
-function Setting({ isDarkMode, toggleSwitch }) { // Receives isDarkMode and toggleSwitch as props
-  const backgroundColor = isDarkMode ? 'darkgrey' : 'white';
-  const textColor = isDarkMode ? 'white' : 'black';
 
-  return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <Text style={[textStyles.titleText, { color: textColor }]}>Settings</Text>
-      <Switch onValueChange={toggleSwitch} value={isDarkMode} />
+function Setting() {
+    const [darkMode, setDarkMode] = useState(dark_mode.value);
+    useEffect(() => {
+        // This effect will be called whenever darkMode changes
+        dark_mode.value = darkMode;
+    }, [darkMode]);
 
-      <StatusBar />
-      <FooterTab />
-    </View>
-  );
+    const toggleSwitch = () => {
+        setDarkMode((previousState) => !previousState);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={textStyles.titleText}>Settings</Text>
+            <Switch onValueChange={toggleSwitch} value={darkMode} />
+
+            <StatusBar />
+            <FooterTab />
+        </View>
+    );
 }
 
 export default Setting;
